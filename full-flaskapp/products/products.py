3e99615cb4e7224c6ded0a48c19405f37563110e
Rahul Shivaprasad <rshiv@rshivapr.local>
2023-04-11 18:36:32 -0500

    pages working

--- full-flaskapp/products/products.py
+++ full-flaskapp/products/products.py
@@ -16,10 +16,12 @@ def translate_game_info(info):
     }
     return game_info
 
-@products_bp.route('/products', methods=['POST'])
+@products_bp.route('/products', methods=['GET'])
 def get_search_term():
-    text = request.form['search'].lower().strip()
+    # text = request.form['search'].lower().strip()
+    text = request.args.get('search', default='', type=str)
     page = request.args.get('page', default=1, type=int)
+    current_app.logger.info('search text : '+text)
     products_per_page = request.args.get('products_per_page', default=20, type=int)
     start_index = (page - 1) * products_per_page
     end_index = start_index + products_per_page
@@ -36,31 +38,29 @@ def get_search_term():
                 # LIMIT %s,%s""".format(text), (start_index, products_per_page))
 
     products = cur.fetchall()
-    current_app.logger.info(products)
-    products = list(map(translate_game_info,products))
-    current_app.logger.info(products)
-    return render_template('products.html',  products=products, total_products=total_products, 
-                           products_per_page=products_per_page, current_page=page)
-
-
-@products_bp.route('/products')
-def products():
-
-    page = request.args.get('page', default=1, type=int)
-    products_per_page = request.args.get('products_per_page', default=20, type=int)
-    start_index = (page - 1) * products_per_page
-    end_index = start_index + products_per_page
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT COUNT(*) FROM Products")
-    total_products = cur.fetchone()[0]
-    cur.execute("SELECT p.productId, p.name, p.imageLink, i.price, AVG(r.rating) FROM Products p NATURAL JOIN Inventory i NATURAL JOIN Reviews r GROUP BY p.productId ORDER BY p.productId LIMIT %s,%s", (start_index, products_per_page))
-    products = cur.fetchall()
-    current_app.logger.info(products)
     products = list(map(translate_game_info,products))
     return render_template('products.html',  products=products, total_products=total_products, 
-                           products_per_page=products_per_page, current_page=page)
+                           products_per_page=products_per_page, current_page=page,search=text)
 
-@products_bp.route('/products/filtered', methods=['POST'])
+
+# @products_bp.route('/products')
+# def products():
+
+#     page = request.args.get('page', default=1, type=int)
+#     products_per_page = request.args.get('products_per_page', default=20, type=int)
+#     start_index = (page - 1) * products_per_page
+#     end_index = start_index + products_per_page
+#     cur = mysql.connection.cursor()
+#     cur.execute("SELECT COUNT(*) FROM Products")
+#     total_products = cur.fetchone()[0]
+#     cur.execute("SELECT p.productId, p.name, p.imageLink, i.price, AVG(r.rating) FROM Products p NATURAL JOIN Inventory i NATURAL JOIN Reviews r GROUP BY p.productId ORDER BY p.productId LIMIT %s,%s", (start_index, products_per_page))
+#     products = cur.fetchall()
+#     current_app.logger.info(products)
+#     products = list(map(translate_game_info,products))
+#     return render_template('products.html',  products=products, total_products=total_products, 
+#                            products_per_page=products_per_page, current_page=page)
+
+@products_bp.route('/products/filtered', methods=['GET'])
 def get_filtered(): 
     page = request.args.get('page', default=1, type=int)
     products_per_page = request.args.get('products_per_page', default=20, type=int)
@@ -71,12 +71,12 @@ def get_filtered():
     total_products = cur.fetchone()[0]
     cur.execute("SELECT p.productId, p.name, p.imageLink, i.price, AVG(r.rating) FROM Products p NATURAL JOIN Inventory i NATURAL JOIN Reviews r WHERE i.price <= (SELECT AVG(price) FROM Inventory i) GROUP BY p.productId ORDER BY p.productId LIMIT %s,%s", (start_index, products_per_page))
     products = cur.fetchall()
-    current_app.logger.info(products)
+    # current_app.logger.info(products)
     products = list(map(translate_game_info,products))
     return render_template('products.html',  products=products, total_products=total_products, 
                            products_per_page=products_per_page, current_page=page)
 
-@products_bp.route('/products/high-rating', methods=['POST'])
+@products_bp.route('/products/high-rating', methods=['GET'])
 def get_high_rated(): 
     page = request.args.get('page', default=1, type=int)
     products_per_page = request.args.get('products_per_page', default=20, type=int)
@@ -93,7 +93,7 @@ HAVING AVG(r.rating) > 4
 ORDER BY p.productId
 LIMIT %s,%s""", (start_index, products_per_page))
     products = cur.fetchall()
-    current_app.logger.info(products)
+    # current_app.logger.info(products)
     products = list(map(translate_game_info,products))
     return render_template('products.html',  products=products, total_products=total_products, 
                            products_per_page=products_per_page, current_page=page)
