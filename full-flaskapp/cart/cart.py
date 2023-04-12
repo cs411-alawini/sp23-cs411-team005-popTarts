deb93a1207038304b83453edfb01f7148daab3d8
Rahul Shivaprasad <rahul.shivaprasad@gmail.com>
2023-04-11 22:07:22 -0500

    everything works hopefully

--- full-flaskapp/cart/cart.py
+++ full-flaskapp/cart/cart.py
@@ -13,6 +13,7 @@ def cart_info(item):
         'id' : item[4]
     }
     return cart_item
+
 @cart_bp.route('/')
 def cart():
     if 'username' not in session:
@@ -26,3 +27,77 @@ def cart():
     cart_items = cur.fetchall()
     cart_items = list(map(cart_info,cart_items))
     return render_template('cart.html', items=cart_items)
+
+@cart_bp.route('/add')
+def add_to_cart(): 
+    id = request.args.get('id')
+    user_id = session['user_id']
+    current_app.logger.info(id)
+    cur = mysql.connection.cursor()
+    cur.execute('select count FROM CartItem c WHERE c.productId = %s AND c.userId = %s', (id, user_id))
+    # query for count
+    #query for either add or update
+    count = cur.fetchone()
+    if count == None:
+        count = 0
+    else:
+        count = count[0]
+    cur.execute('SELECT MAX(itemNumber) FROM CartItem WHERE userId = %s',(user_id,)); 
+    itemNum = cur.fetchone()
+    if itemNum[0] == None:
+        itemNum = 0
+    else:
+        current_app.logger.info('retrieve itemnum:{}'.format(itemNum))
+        itemNum = int(itemNum[0])
+
+    count+=1
+    if count==1:
+        cur.execute('INSERT INTO CartItem(itemNumber, userId, productId, count) VALUES (%s,%s,%s,%s)',(itemNum+1,user_id,id,count));
+    else:
+         cur.execute('''UPDATE CartItem
+                    SET count = %s 
+                    WHERE userId = %s AND productId = %s;''',
+                    (count,user_id,id));
+    mysql.connection.commit()
+    return redirect('/cart')
+    
+@cart_bp.route('/subtract')
+def subtract_from_cart():
+    id = request.args.get('id')
+    user_id = session['user_id']
+    current_app.logger.info(id)
+    cur = mysql.connection.cursor()
+    cur.execute('select count FROM CartItem c WHERE c.productId = %s AND c.userId = %s', (id, user_id))
+    count = cur.fetchone()
+    if count == None:
+        return redirect('/cart')
+    else:
+        count = count[0]-1
+    if count==0:
+        cur.execute('''DELETE FROM CartItem
+            WHERE productId = %s AND userID = %s;''',
+            (id,user_id));
+    else:
+        cur.execute('''UPDATE CartItem
+            SET count = %s 
+            WHERE userId = %s AND productId = %s;''',
+            (count,user_id,id));
+    mysql.connection.commit()
+    #query for count
+    #query for either update or delete 
+    return redirect('/cart')
+
+    
+@cart_bp.route('/delete')
+def delete_from_cart():
+    id = request.args.get('id')
+    user_id = session['user_id']
+    current_app.logger.info(id)
+    cur = mysql.connection.cursor()
+    cur.execute('''DELETE FROM CartItem
+        WHERE productId = %s AND userID = %s;''',
+        (id,user_id));
+    mysql.connection.commit()
+    #query for count
+    #query for either update or delete 
+    return redirect('/cart')
