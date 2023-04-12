a23bc935af46c181a570a2d0f4f97894ae563751
Rahul Shivaprasad <rshiv@rshivapr.local>
2023-04-11 19:53:29 -0500

    pages linked

--- full-flaskapp/products/products.py
+++ full-flaskapp/products/products.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template,request,current_app
+from flask import Blueprint, render_template,request,current_app, redirect, url_for, session
 import csv 
 from db import mysql
 
@@ -12,12 +12,15 @@ def translate_game_info(info):
         'name': info[1],
         'imageLink':info[2],
         'price': info[3],
-        'rating': info[4]
+        'rating': info[4],
+        'discount': info[5]
     }
     return game_info
 
 @products_bp.route('/products', methods=['GET'])
 def get_search_term():
+    if 'username' not in session:
+        return redirect(url_for('login.login'))
     # text = request.form['search'].lower().strip()
     text = request.args.get('search', default='', type=str)
     page = request.args.get('page', default=1, type=int)
@@ -28,7 +31,7 @@ def get_search_term():
     cur = mysql.connection.cursor()
     cur.execute("SELECT COUNT(*) FROM Products")
     total_products = cur.fetchone()[0]
-    cur.execute("""SELECT p.productId, p.name, p.imageLink, i.price, AVG(r.rating)
+    cur.execute("""SELECT p.productId, p.name, p.imageLink, i.price, AVG(r.rating), i.discount
                 FROM Products p LEFT JOIN Inventory i ON p.productId = i.productId 
                 LEFT JOIN Reviews r ON p.productId = r.productId WHERE name LIKE '%%{}%%' 
                 GROUP BY p.productId ORDER BY p.productId LIMIT %s,%s""".format(text), (start_index, products_per_page))
@@ -62,6 +65,8 @@ def get_search_term():
 
 @products_bp.route('/products/filtered', methods=['GET'])
 def get_filtered(): 
+    if 'username' not in session:
+        return redirect(url_for('login.login'))
     page = request.args.get('page', default=1, type=int)
     products_per_page = request.args.get('products_per_page', default=20, type=int)
     start_index = (page - 1) * products_per_page
@@ -69,7 +74,7 @@ def get_filtered():
     cur = mysql.connection.cursor()
     cur.execute("SELECT COUNT(*) FROM Products")
     total_products = cur.fetchone()[0]
-    cur.execute("SELECT p.productId, p.name, p.imageLink, i.price, AVG(r.rating) FROM Products p NATURAL JOIN Inventory i NATURAL JOIN Reviews r WHERE i.price <= (SELECT AVG(price) FROM Inventory i) GROUP BY p.productId ORDER BY p.productId LIMIT %s,%s", (start_index, products_per_page))
+    cur.execute("SELECT p.productId, p.name, p.imageLink, i.price, AVG(r.rating), i.discount FROM Products p NATURAL JOIN Inventory i NATURAL JOIN Reviews r WHERE i.price <= (SELECT AVG(price) FROM Inventory i) GROUP BY p.productId ORDER BY p.productId LIMIT %s,%s", (start_index, products_per_page))
     products = cur.fetchall()
     # current_app.logger.info(products)
     products = list(map(translate_game_info,products))
@@ -78,6 +83,8 @@ def get_filtered():
 
 @products_bp.route('/products/high-rating', methods=['GET'])
 def get_high_rated(): 
+    if 'username' not in session:
+        return redirect(url_for('login.login'))
     page = request.args.get('page', default=1, type=int)
     products_per_page = request.args.get('products_per_page', default=20, type=int)
     start_index = (page - 1) * products_per_page
@@ -85,7 +92,7 @@ def get_high_rated():
     cur = mysql.connection.cursor()
     cur.execute("SELECT COUNT(*) FROM Products")
     total_products = cur.fetchone()[0]
-    cur.execute("""SELECT p.productId, p.name, p.imageLink, i.price, AVG(r.rating) AS Average_Rating FROM Products p NATURAL JOIN Inventory i JOIN Reviews r 
+    cur.execute("""SELECT p.productId, p.name, p.imageLink, i.price, AVG(r.rating) AS Average_Rating, i.discount FROM Products p NATURAL JOIN Inventory i JOIN Reviews r 
 ON (p.productId = r.productId)
 WHERE i.supply > 0
 GROUP BY p.productId
